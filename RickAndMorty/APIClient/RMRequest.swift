c5f4ecb97d37305fd423406eb87e70b17e694273
NZhukov <nzhukov.rnd@gmail.com>
2023-08-09 17:05:58 +0400

    fix api

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -7,12 +7,6 @@
 
 import Foundation
 
-final class RMService {
-    static let shared = RMService()
+final class RMRequest {
     
-    private init() {}
-    
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
-        
-    }
 }
